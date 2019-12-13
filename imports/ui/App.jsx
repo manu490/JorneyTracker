a76baa3fdcfb67d32a21622313ac435d25469611
manu490 <13869998+manu490@users.noreply.github.com>
2019-12-13 10:38:17 +0530

    Initial commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Hello from './Hello.jsx';
+
+const App = () => (
+  <div>
+    <h1>Welcome!</h1>
+    <Hello />
+  </div>
+);
+
+export default App;
---
+++ imports/ui/Hello.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <p>Beginning of My journey.</p>
+      </div>
+    );
+  }
+}
