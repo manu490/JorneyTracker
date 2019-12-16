fdddfe9782d2268a29fe177fbfdaa1c79d38d981
manu490 <13869998+manu490@users.noreply.github.com>
2019-12-16 19:49:00 +0530

    .

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 import Header from './header.jsx';
 import {BrowserRouter as Router, Redirect, Switch, Route, Link}  from "react-router-dom";
 import Body from './body.jsx';
-import Footer from './footer.jsx';
+
 
 const App = () => (
 
   <span>
     <Header />
     <Body /><br/><br/><br/><br/><br/>
-    <Footer />
   </span>
 );
 
