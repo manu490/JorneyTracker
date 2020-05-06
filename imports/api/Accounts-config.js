b5d43616f3f1012469f935b36bad4ec3851a9b3c
Manu Ram <13869998+manu490@users.noreply.github.com>
2020-05-06 11:57:32 +0530

    Update Accounts-config.js

--- imports/api/Accounts-config.js
+++ imports/api/Accounts-config.js
@@ -2,7 +2,7 @@ import { Accounts } from 'meteor/accounts-base';
 
 
 AccountsTemplates.configure({
-    forbidClientAccountCreation : true,
+    forbidClientAccountCreation : false,
 
 });
 /*Accounts.onLogin(function(){
